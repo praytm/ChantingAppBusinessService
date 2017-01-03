51850c1e5a7b78cfe36c17a7196653b557978d3e
er_aditya_anand <er_aditya_anand>
2017-01-03 19:51:27 +0530

    business service layer

---
+++ src/main/java/org/iskcon/nvcc/chantingApp/bs/UserService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.iskcon.nvcc.chantingApp.bs;
+
+import org.iskcon.nvcc.chantingApp.dto.UserDTO;
+
+/**
+ * @author aditya.anand
+ *
+ */
+public interface UserService {
+
+	
+	public UserDTO registerUser(UserDTO userDto);
+}

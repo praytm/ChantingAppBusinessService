a3645c1c2a8052d1af703acca1fbd5f10a6c4345
Rajat10_10 <iamrajat10cube@gmail.com>
2018-09-16 23:07:11 +0530

    City & Country

--- src/main/java/org/iskcon/nvcc/chantingApp/dto/UserDTO.java
+++ src/main/java/org/iskcon/nvcc/chantingApp/dto/UserDTO.java
@@ -20,6 +20,33 @@ public class UserDTO implements Serializable{
 	private Boolean isRegisteredViaGoogle;
 	private String profilepic;
 	private String googleAuthToken;
+	private String city;
+	private String country;
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
 	
 	/**
 	 * @return the password
